051f77d28567e9eb5b2d23fac004d9f7f1b1637f
Daniel Toomey <daniel_toomey@hotmail.com>
2019-03-02 23:57:22 +1000

    Tidied up the PS scripts to make them easier to use

--- Custom Scripts/SF_DeployApplication.ps1
+++ Custom Scripts/SF_DeployApplication.ps1
@@ -4,38 +4,53 @@
 
 Import-Module "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\Tools\PSModule\ServiceFabricSDK\ServiceFabricSDK.psm1"
 
-$path = "C:\Repos\Voting_v2\Voting_v2\pkg\Debug" #Set this to the path for your solution output directory
+###################################################################################################
+$path = "C:\Repos\Demos\Voting_v3\Voting\pkg\Debug" #Set this to the path for your solution output directory
 $imageStorePath = "Voting"
 $appTypeName = "VotingType"
 $appName = "fabric:/$imageStorePath"
 $appVersion = "1.0.0"
-$ServerCommonName = "CLUSTER_NAME.REGION.cloudapp.azure.com"   #for azure hosted cluster deployments
-#$ServerCommonName = "localhost"                               #for local hosted cluster deployments
+#$ServerCommonName = "CLUSTER_NAME.REGION.cloudapp.azure.com"   #for azure hosted cluster deployments
+$ServerCommonName = "localhost"                               #for local hosted cluster deployments
 $clusterAddress = "${ServerCommonName}:19000"
 $thumb="YOUR_CERTIFICATE_THUMBPRINT"   #for secured cluster hosted in azure
+###################################################################################################
 
 Write-Output "Deploying application '${imageStorePath}' to cluster at '${clusterAddress}'..."
 
-# Connect to the Service Fabric cluster  (need certificate for a secured cluster in Azure)
-#Connect-ServiceFabricCluster $clusterAddress  #for local cluster only
-Connect-ServiceFabricCluster -ConnectionEndpoint $clusterAddress -X509Credential -ServerCertThumbprint $thumb -FindType FindByThumbprint -FindValue $thumb -StoreLocation CurrentUser -StoreName My
+try {
 
-$imageStoreConnStr = Get-ImageStoreConnectionStringFromClusterManifest(Get-ServiceFabricClusterManifest)
+    # Connect to the Service Fabric cluster  (need certificate for a secured cluster in Azure)
+    if($ServerCommonName -eq "localhost")
+    {
+	    Connect-ServiceFabricCluster $clusterAddress
+    }
+    else
+    {
+	    Connect-ServiceFabricCluster -ConnectionEndpoint $clusterAddress -X509Credential -ServerCertThumbprint $thumb -FindType FindByThumbprint -FindValue $thumb -StoreLocation CurrentUser -StoreName My
+    }
 
-# Upload package to package store
-Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $path -ApplicationPackagePathInImageStore $imageStorePath -TimeoutSec 1800
+    $imageStoreConnStr = Get-ImageStoreConnectionStringFromClusterManifest(Get-ServiceFabricClusterManifest)
 
-# Register the application package
-Register-ServiceFabricApplicationType -ApplicationPathInImageStore $imageStorePath
+    # Upload package to package store
+    Copy-ServiceFabricApplicationPackage -ApplicationPackagePath $path -ApplicationPackagePathInImageStore $imageStorePath -TimeoutSec 1800
 
-# Get all application types registered in the cluster
-#Get-ServiceFabricApplicationType
+    # Register the application package
+    Register-ServiceFabricApplicationType -ApplicationPathInImageStore $imageStorePath
 
-# Create the application 
-New-ServiceFabricApplication $appName $appTypeName $appVersion
+    # Get all application types registered in the cluster
+    #Get-ServiceFabricApplicationType
 
-# Get all application instances created in the cluster
-Get-ServiceFabricApplication -ApplicationName $appName
+    # Create the application 
+    New-ServiceFabricApplication $appName $appTypeName $appVersion
 
-# Get all service instances for each application
-Get-ServiceFabricApplication -ApplicationName $appName | Get-ServiceFabricService
+    # Get all application instances created in the cluster
+    Get-ServiceFabricApplication -ApplicationName $appName
+
+    # Get all service instances for each application
+    Get-ServiceFabricApplication -ApplicationName $appName | Get-ServiceFabricService
+}
+catch
+{
+    Write-Output "An error occurred: " $Error[0]
+}
--- Custom Scripts/SF_UndeployApplication.ps1
+++ Custom Scripts/SF_UndeployApplication.ps1
@@ -1,29 +1,45 @@
 ###################################################################################################
-##  REF httpsdocs.microsoft.comen-usazureservice-fabricservice-fabric-automate-powershell
+##  REF: https://docs.microsoft.com/en-us/azure/service-fabric/service-fabric-automate-powershell
 ###################################################################################################
 
-Import-Module $ENVProgramFilesMicrosoft SDKsService FabricToolsPSModuleServiceFabricSDKServiceFabricSDK.psm1
+Import-Module "$ENV:ProgramFiles\Microsoft SDKs\Service Fabric\Tools\PSModule\ServiceFabricSDK\ServiceFabricSDK.psm1"
 
+###################################################################################################
 $imageStorePath = 'Voting'
 $appTypeName = 'VotingType'
-$appName = fabric$imageStorePath
+$appName = "fabric:/$imageStorePath"
 $appVersion = '1.0.0'
-$ServerCommonName = "CLUSTER_NAME.REGION.cloudapp.azure.com"   #for azure hosted cluster deployments
-#$ServerCommonName = localhost                               #for local hosted cluster deployments
-$clusterAddress = ${ServerCommonName}19000
+#$ServerCommonName = "CLUSTER_NAME.REGION.cloudapp.azure.com"   #for azure hosted cluster deployments
+$ServerCommonName = "localhost"								   #for local hosted cluster deployments
+$clusterAddress = "${ServerCommonName}:19000"
 $thumb="YOUR_CERTIFICATE_THUMBPRINT"   #for secured cluster hosted in azure
+###################################################################################################
 
-# Connect to the Service Fabric cluster  (need certificate for a secured cluster in Azure)
-#Connect-ServiceFabricCluster $clusterAddress  #for local cluster only
-Connect-ServiceFabricCluster -ConnectionEndpoint $clusterAddress -X509Credential -ServerCertThumbprint $thumb -FindType FindByThumbprint -FindValue $thumb -StoreLocation CurrentUser -StoreName My
+try {
+
+    # Connect to the Service Fabric cluster  (need certificate for a secured cluster in Azure)
+    if($ServerCommonName -eq "localhost")
+    {
+	    Connect-ServiceFabricCluster $clusterAddress
+    }
+    else
+    {
+	    Connect-ServiceFabricCluster -ConnectionEndpoint $clusterAddress -X509Credential -ServerCertThumbprint $thumb -FindType FindByThumbprint -FindValue $thumb -StoreLocation CurrentUser -StoreName My
+    }
  
-Write-Output Removing application '${imageStorePath}' from cluster at '${clusterAddress}'...
+    Write-Output Removing application '${imageStorePath}' from cluster at '${clusterAddress}'...
 
-# Remove an application instance
-Remove-ServiceFabricApplication $appName -Force  #force flag skips prompt for confirmation
+    # Remove an application instance
+    Remove-ServiceFabricApplication $appName -Force  #force flag skips prompt for confirmation
 
-# Unregister the application type
-Unregister-ServiceFabricApplicationType $appTypeName $appVersion
+    # Unregister the application type
+    Unregister-ServiceFabricApplicationType $appTypeName $appVersion
 
-# Remove the application package
-Remove-ServiceFabricApplicationPackage -ApplicationPackagePathInImageStore Voting
+    # Remove the application package
+    Remove-ServiceFabricApplicationPackage -ApplicationPackagePathInImageStore Voting
+
+}
+catch
+{
+    Write-Output "An error occurred: " $Error[0]
+}
