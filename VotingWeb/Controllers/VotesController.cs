71eeb6f1f0e148b842f136bf0b2315feb8fb1d7a
dtoomey <daniel_toomey@hotmail.com>
2020-02-09 12:16:58 +1000

    Comment edit only to trigger build

--- VotingWeb/Controllers/VotesController.cs
+++ VotingWeb/Controllers/VotesController.cs
@@ -94,7 +94,7 @@ namespace VotingWeb.Controllers
             StringContent putContent = new StringContent($"{{ 'name' : '{name}' }}", Encoding.UTF8, "application/json");
             putContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
-            // Insert "trump" snippet here to introduce a bug
+            // Insert "trump" snippet here to introduce a nasty bug
             
             // Record the new vote
             using (HttpResponseMessage response = await this.httpClient.PutAsync(proxyUrl, putContent))
